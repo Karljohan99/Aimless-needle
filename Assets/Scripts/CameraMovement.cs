aa2e7a8f09a4ecd11fdccae11c6424e5a1d042d5
Kätlin <k2tu99@gmail.com>
2020-10-03 12:53:08 +0300

    camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
